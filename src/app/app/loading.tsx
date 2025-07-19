b0ac0449fb2a3bd79ad8a21e67ef37fa90e845a9
Ekrem Sönmezer <ekremsonmezer@gmail.com>
2025-07-20 01:35:55 +0300

    add loading component

---
+++ src/app/app/loading.tsx
@@ -0,0 +1,10 @@
+import {HugeiconsIcon} from "@hugeicons/react";
+import {Loading02Icon} from "@hugeicons/core-free-icons";
+
+export default function Loading() {
+    return (
+        <div className="flex h-screen py-48 border justify-center items-center">
+            <HugeiconsIcon icon={Loading02Icon} size={32} className="mr-2 animate-spin text-primary" />
+        </div>
+    )
+}
